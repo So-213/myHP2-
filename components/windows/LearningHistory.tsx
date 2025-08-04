b6779fba874e94754dadb7f9a6e022d265d6ac2a
So-213 <betepp7@gmail.com>
2025-08-04 10:28:26 +0800

    8/4

--- components/windows/LearningHistory.tsx
+++ components/windows/LearningHistory.tsx
@@ -1,56 +1,60 @@
 export default function LearnigHistory() {
+  return (
+    <div className="space-y-4 flex flex-col items-start">
 
 
 
-    return (
-      <div className="space-y-4 flex flex-col items-start">
+      <h1 className="text-l font-bold">学習開始：2024.9.15</h1>
 
-        <h1 className="text-l font-bold">学習開始：2024.9.15</h1>
-        <h1 className="text-l font-bold">学習教材：Udemy / Youtube</h1>
 
+      <div className="flex-col w-full">
         <div className="flex-col w-full">
-          <h1 className="text-xl font-bold">1-3ヶ月目</h1>
+          <h2 className="text-base font-bold">1-3ヶ月目</h2>
           <p>GitHub / Web技術概略</p>
           <p>HTML / CSS / JS</p>
           <p>JS / Node.js / React</p>
         </div>
 
         <div className="flex-col w-full">
-          <h1 className="text-xl font-bold">4-5ヶ月目</h1>
+          <h2 className="text-base font-bold">4-5ヶ月目</h2>
           <p>API / Projects①作成</p>
           <p>Vercel / V0 / 本HP作成</p>
           <p>cookie / セッション / 認証認可</p>
         </div>
 
         <div className="flex-col w-full">
-          <h1 className="text-xl font-bold">6ヶ月目</h1>
+          <h2 className="text-base font-bold">6ヶ月目</h2>
           <p>Projects②作成開始</p>
           <p>Cognito / Route53</p>
           <p>Next.js</p>
         </div>
 
         <div className="flex-col w-full">
-          <h1 className="text-xl font-bold">7ヶ月目</h1>
+          <h2 className="text-base font-bold">7ヶ月目</h2>
           <p>Auth.js（JWTベース認証）</p>
           <p>DB設計 / Postgres / Prisma / Supabase</p>
         </div>
 
         <div className="flex-col w-full">
-          <h1 className="text-xl font-bold">8-9ヶ月目（KL渡航）</h1>
-          <p>休止期間</p>
+          <h2 className="text-base font-bold">8-9ヶ月目（KL渡航期）</h2>
+          <p>休止期間2.5ヶ月</p>
         </div>
 
         <div className="flex-col w-full">
-          <h1 className="text-xl font-bold">9ヶ月目</h1>
-          <p>休止期間</p>
-        </div>
-
-        <div className="flex-col w-full">
-          <h1 className="text-xl font-bold">10ヶ月目</h1>
+          <h2 className="text-base font-bold">10ヶ月目</h2>
           <p>Stripe</p>
         </div>
-
       </div>
-    );
-  }
+
+
+      <h1 className="text-l font-bold">コメント</h1>
+      <p>
+        基本的に新しい技術を学んで実装する際は、UdemyやYoutubeで概要やロジックを理解してからコードはCursorに書いてもらうという流れでした。
+      </p>
+
+
+
+    </div>
+  );
+}
   
